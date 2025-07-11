a88e91a6c7baad418c3f88cdf80235d8820fc6c3
rithikas05 <165898466+rithikas05@users.noreply.github.com>
2025-07-11 18:55:14 +0530

    Setup CORS with CLIENT_URL for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,13 @@ import noteRoutes from "./routes/noteRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
